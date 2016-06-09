9d03fdb956d68f096c0942432c1f5c89a34dc5f7
Vedhavalli S <Vedhavalli.S@cognizant.com>
2016-06-09 19:12:22 +0530

    merged css tweaks with function works

--- hackathon/app/app.js
+++ hackathon/app/app.js
@@ -1,2 +1,26 @@
-var hackathonApp = angular.module('hackathon', []);
+var hackathonApp = angular.module('hackathon', ['ui.router']);
+
+hackathonApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/homePage');
+    
+    $stateProvider.state('homePage', {
+    	    url: '/homePage',
+    		templateUrl: 'templates/homePage.html',
+            controller: 'indexController'
+            
+            
+        })
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('landingPage', {
+             url: '/landingPage',
+            templateUrl: 'templates/landingPage.html',
+            controller: 'landingController'
+            
+
+        });
+        
+});
+
 
